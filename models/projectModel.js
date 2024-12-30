0f9b1fa9f018ae0dbc9eaddff3502b924d3b4514
Shruti Maurya <shrutimaurya782@gmail.com>
2024-12-30 16:28:52 +0530

    init commit

--- models/projectModel.js
+++ models/projectModel.js
@@ -4,6 +4,7 @@ const projectSchema = new mongoose.Schema({
     projectName: { type: String, required: true },
     madeBy: { type: String, required: true },
     toolsUsed: { type: [String], required: true }, 
+    githublink:{ type: String, required: true },
 });
 
 module.exports = mongoose.model('Project', projectSchema);
