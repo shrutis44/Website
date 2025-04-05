36172683e15d53c9296ec03f98e9ab31ed5d1087
Shruti Maurya <shrutimaurya782@gmail.com>
2025-04-05 23:22:58 +0530

    init commit

--- services/contactService.js
+++ services/contactService.js
@@ -1,10 +1,49 @@
+// const Contact = require('../models/contactModel');
+
+// exports.saveContact = async (contactData) => {
+//     const contact = new Contact(contactData);
+//     return await contact.save();
+// };
+
+// exports.fetchAllContacts = async () => {
+//     return await Contact.find();
+// };
+
 const Contact = require('../models/contactModel');
+const nodemailer = require('nodemailer');
 
 exports.saveContact = async (contactData) => {
     const contact = new Contact(contactData);
-    return await contact.save();
+    await contact.save();
+
+    await sendEmail(contactData);
+
+    return contact;
 };
 
-exports.fetchAllContacts = async () => {
-    return await Contact.find();
+const sendEmail = async (contactData) => {
+    try {
+        const transporter = nodemailer.createTransport({
+            service: 'gmail', 
+            auth: {
+                user: process.env.EMAIL_USER,
+                pass: process.env.EMAIL_PASS
+            }
+        });
+        const mailOptions = {
+            from: process.env.EMAIL_USER,
+            to: process.env.RECEIVER_EMAIL, 
+            subject: 'New Contact Form Submission',
+            text: `You have a new contact request:\n\nName: ${contactData.Name}\nEmail: ${contactData.email}\nPhone: ${contactData.phone}\nMessage: ${contactData.message}`
+        };
+
+    
+        await transporter.sendMail(mailOptions);
+        console.log('Email sent successfully.');
+    } catch (error) {
+        console.error('Error sending email:', error);
+    }
 };
+
+
+
