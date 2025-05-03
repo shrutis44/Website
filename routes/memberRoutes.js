826932b34263a48aca7bd712a257d8201103f1b5
Shruti Maurya <shrutimaurya782@gmail.com>
2025-05-03 22:36:07 +0530

    init commit

--- routes/memberRoutes.js
+++ routes/memberRoutes.js
@@ -15,11 +15,24 @@ const express = require("express");
 const memberController = require("../controllers/user");
 const getUpload = require("../middlewares/multer");
 
-const upload = getUpload("members"); // Folder name for members
+const upload = getUpload("members"); 
 
 const router = express.Router();
 
 router.post("/members", upload.single("profile"), memberController.addMember);
+router.post("/members/2ndyear", upload.single("profile"), (req, res, next) => {
+    req.body.year = "2ndyear";
+    memberController.addMember(req, res, next);
+  });
+  router.post("/members/3rdyear", upload.single("profile"), (req, res, next) => {
+    req.body.year = "3rdyear";
+    memberController.addMember(req, res, next);
+  });
+  router.post("/members/4thyear", upload.single("profile"), (req, res, next) => {
+    req.body.year = "4thyear";
+    memberController.addMember(req, res, next);
+  });
+   
 router.get("/members", memberController.getAllMembers);
 router.get("/members/:year", memberController.getMembersByYear);
 
